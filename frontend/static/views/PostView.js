5d1d5887beee0a487861e458e9636790d11c3013
12ayo22 <12ayo22@gmail.com>
2023-01-16 22:05:27 +0900

    💄 PostView UI 추가

--- frontend/static/views/PostView.js
+++ frontend/static/views/PostView.js
@@ -2,11 +2,12 @@ import AbstractView from './AbstractView.js';
 
 export default class extends AbstractView {
   constructor(params) {
-    super();
+    super(params);
     this.setTitle('Posts');
   }
 
   async getHtml() {
+    console.log(this.params.id);
     return `
       <h1>Welcome back, Posts</h1>
       <p> Dash Board!</p>
