3fe97901661eec169aec2af3aae3af774b1d706c
12ayo22 <12ayo22@gmail.com>
2023-01-16 21:11:11 +0900

    ✨ add server static assets directory

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const path = require('path');
 
 const app = express();
 
+// static 경로 설정하기
+app.use(
+  '/static',
+  express.static(path.resolve(__dirname, 'frontend', 'static'))
+);
+
 // SPA기 때문에 index.html만 사용한다.
+// static 디렉토리를 사용해야하는데 아래 코드 때문에 모든 경로가
+// index.html을 반환하게 된다. -> 따라서 static 경로 설정해야한다.
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve('frontend', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
 });
 
 app.listen(3000, () => {
